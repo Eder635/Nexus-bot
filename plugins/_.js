a5ac9c45ef7bc1dd034f390e79cd29442808d036
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-07-02 18:47:43 -0500

    Actualizar _.js

--- plugins/_.js
+++ plugins/_.js
@@ -41,7 +41,7 @@ rows: [{ header: "Menu Completo", title: "", id: `.allmenu`, description: `Para 
 { header: "Play", title: "", id: `.play`, description: `Descarga tus musicas favoritas 🎧\n` },
 { header: "creador", title: "", id: `.owner`, description: `Contacta a mi creador` }
 ]})
-await conn.sendList(m.chat, '👋🏻 Hola¡! Bienvenido A Mi Sub Menú\n\n*Creador:* Jxtxn17\n*Versión:* ${vs}\n\n si hay algún error puedes contactarme, usa el comando: .owner\n\nGracias¡! 🔴', null, `Selecione la opción correcta`, listSections, { mentions: [m.sender]}, {quoted: m})
+await conn.sendList(m.chat, '👋🏻 Hola¡! Bienvenido A Mi Sub Menú\n\n*Creador:* Jxtxn17\n*Versión:* 1.0.0\n*Pais:* ${formatDate}\n\n si hay algún error puedes contactarme, usa el comando: .owner\n\nGracias¡! 🔴', null, `Selecione la opción correcta`, listSections, { mentions: [m.sender]}, {quoted: m})
 
 //await conn.reply(m.chat, menu, a, { contextInfo: { externalAdReply: {title: '👋 ¡Hola!', body: saludo, sourceUrl: ig, thumbnail: await (await fetch(pp)).buffer() }}})
 
