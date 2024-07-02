f5a2771cbf863c1d768e88d3ad470b5dd31aa5b0
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-07-02 14:23:14 -0500

    Actualizar own-banchat.js

--- plugins/own-banchat.js
+++ plugins/own-banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m) => {
 
 global.db.data.chats[m.chat].isBanned = true
-conn.reply(m.chat, `✅ *Este chat fue baneado con éxito*`, m, fake, )
+conn.reply(m.chat, `✅ *Este chat fue baneado con éxito, ys no pueden usar a BaileyBot-MD*`, m, fake, )
 
 }
 handler.help = ['banchat']
